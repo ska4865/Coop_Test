031e7a22ffb176668b11079df1e575ab4ee79a6b
Sakai Alexander <ska4865@rit.edu>
2022-09-02 11:01:40 -0400

    Tickets remaining and to be chosen visible and working

--- src/GUI/Cust.java
+++ src/GUI/Cust.java
@@ -7,12 +7,27 @@ package GUI;
 import Sales.Tickets;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Map;
 
-public class Cust extends Frame{
-    Tickets tix = new Tickets();
-    public void movieView(){
+public class Cust extends Frame {
+
+    protected Tickets tix;
+    private List numTickets;
+    private Map<String, Integer> screenInfo;
+
+    public Tickets init() {
+        tix = new Tickets();
+        String[] movieNames = {"Ponyo", "Bleach", "Fast&Furious", "2012", "Scream 2"};
+        tix.swapMovies(movieNames);
+        screenInfo = tix.getInfo();
+        return tix;
+    }
+
+    public void movieView() {
 
         Label prompt = new Label("Please choose what movie you would like to see below: ");
 
@@ -23,7 +38,8 @@ public class Cust extends Frame{
         Button screen4 = new Button(tix.getSCREEN_4());
         Button screen5 = new Button(tix.getSCREEN_5());
 
-        List numTickets = new List(10);
+        Label ticketLabel = new Label("# of Tickets you desire:");
+        numTickets = new List(6);
         numTickets.add("1");
         numTickets.add("2");
         numTickets.add("3");
@@ -32,14 +48,15 @@ public class Cust extends Frame{
 
         // setting button position on screen
         prompt.setBounds(100, 50, 500, 50);
-        screen1.setBounds(60,200,80,30);
-        screen2.setBounds(160,200,80,30);
-        screen3.setBounds(260,200,80,30);
-        screen4.setBounds(360,200,80,30);
-        screen5.setBounds(460,200,80,30);
-        numTickets.setBounds(300,300,80,50);
+        screen1.setBounds(60, 200, 80, 30);
+        screen2.setBounds(160, 200, 80, 30);
+        screen3.setBounds(260, 200, 80, 30);
+        screen4.setBounds(360, 200, 80, 30);
+        screen5.setBounds(460, 200, 80, 30);
+        numTickets.setBounds(330, 300, 50, 50);
+        ticketLabel.setBounds(300, 260, 150, 50);
 
-        // adding button into frame
+        // adding components to frame
         add(screen1);
         add(screen2);
         add(screen3);
@@ -47,13 +64,30 @@ public class Cust extends Frame{
         add(screen5);
         add(prompt);
         add(numTickets);
+        add(ticketLabel);
+
+        //commands to keep track of what ticket is being used for what movie
+        screen1.setActionCommand(tix.getSCREEN_1());
+        screen2.setActionCommand(tix.getSCREEN_2());
+        screen3.setActionCommand(tix.getSCREEN_3());
+        screen4.setActionCommand(tix.getSCREEN_4());
+        screen5.setActionCommand(tix.getSCREEN_5());
+
+        //Adding listeners to track events
+        screen1.addActionListener(new ButtonClickListener());
+        screen2.addActionListener(new ButtonClickListener());
+        screen3.addActionListener(new ButtonClickListener());
+        screen4.addActionListener(new ButtonClickListener());
+        screen5.addActionListener(new ButtonClickListener());
+        numTickets.addActionListener(new ButtonClickListener());
+
 
 
         // frame size 300 width and 300 height
-        setSize(700,500);
+        setSize(700, 500);
 
         // setting the title of Frame
-        setTitle("Welcome to the Theaters!");
+        setTitle("Welcome to the Movie Theater!");
 
         // no layout manager
         setLayout(null);
@@ -70,14 +104,39 @@ public class Cust extends Frame{
         });
     }
 
-    public void init(){
-        String[] movieNames = {"Ponyo", "Bleach", "Fast&Furious", "2012","Scream 2"};
-        tix.swapMovies(movieNames);
-    }
-
     public static void main(String[] args) {
         Cust start = new Cust();
         start.init();
         start.movieView();
+    }
+
+    public class ButtonClickListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String name = e.getActionCommand();
+            Label label = new Label();
+            if (name.equals(tix.getSCREEN_1())) {
+                System.out.print(screenInfo);
+                label.setText("Amount of Tickets left: " +
+                        screenInfo.get(tix.getSCREEN_1()).toString());
+                numTickets.getSelectedItem();
+
+
+            } else if (name.equals(tix.getSCREEN_2())) {
+                label.setText("df");
+            } else if (name.equals(tix.getSCREEN_3())) {
+                label.setText("asd");
+            } else if (name.equals(tix.getSCREEN_4())) {
+                label.setText("omv");
+            } else {
+                label.setText("five");
+            }
+            label.setBounds(150, 300, 150, 50);
+
+            remove(label);
+            add(label);
+
+        }
     }
 }
--- src/Sales/Tickets.java
+++ src/Sales/Tickets.java
@@ -1,22 +1,33 @@
 package Sales;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tickets {
 
-    private static int MAX_TICKETS = 5;
+    private static Integer MAX_TICKETS = 5;
     private int totalSales = 0;
     private String SCREEN_1;
     private String SCREEN_2;
     private String SCREEN_3;
     private String SCREEN_4;
     private String SCREEN_5;
+    private Map<String, Integer> screensInfo;
 
     public void swapMovies(String[] movieName){
+        screensInfo = new HashMap<String, Integer>(){{
+            put(movieName[0], MAX_TICKETS);
+            put(movieName[1], MAX_TICKETS);
+            put(movieName[2], MAX_TICKETS);
+            put(movieName[3], MAX_TICKETS);
+            put(movieName[4], MAX_TICKETS);
+        }};
+
         SCREEN_1 = movieName[0];
         SCREEN_2 = movieName[1];
         SCREEN_3 = movieName[2];
         SCREEN_4 = movieName[3];
         SCREEN_5 = movieName[4];
-
     }
 
     public void ticketSale(int amount){
@@ -41,5 +52,8 @@ public class Tickets {
     }
     public String getSCREEN_5() {
         return SCREEN_5;
+    }
+    public Map<String, Integer> getInfo(){
+        return screensInfo;
     }
 }
